87c34a07fc966742d81c0a4601ce85c42df9f4c5
edguru <72596185+edguru@users.noreply.github.com>
2020-12-23 03:53:13 +0530

    Update __init__.py

--- naruto/__init__.py
+++ naruto/__init__.py
@@ -28,6 +28,7 @@ USERBOT_LOAD =""
 USERBOT_NOLOAD = ""
 ASSISTANT_LOAD = ""
 ASSISTANT_NOLOAD = ""
+Command = Config.command
 # Postgresql
 def mulaisql() -> scoped_session:
     global DB_AVAILABLE
