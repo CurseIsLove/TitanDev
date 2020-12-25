bc0b0ae48c70d57634e868292128a1fe05152f2d
edguru <72596185+edguru@users.noreply.github.com>
2020-12-26 03:24:36 +0530

    Update config.py

--- naruto/config.py
+++ naruto/config.py
@@ -69,4 +69,4 @@ class Config:
     HEROKU_APP = heroku3.from_key(HEROKU_API_KEY).apps()[HEROKU_APP_NAME] \
         if HEROKU_API_KEY and HEROKU_APP_NAME else None
     STATUS = None
-    PM_PERMIT= (os.environ.get("PM_PERMIT",None)
+    PM_PERMIT= (os.environ.get("PM_PERMIT",None))
