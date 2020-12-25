9cccbf9e4e834bdbf9084eb5e7736befcb799700
edguru <72596185+edguru@users.noreply.github.com>
2020-12-26 04:00:41 +0530

    Update pm.py

--- naruto/plugins/pm.py
+++ naruto/plugins/pm.py
@@ -3,11 +3,11 @@ import re
 from pyrogram import filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
-from nana import app, setbot, Command, Owner, BotUsername, DB_AVAILABLE, AdminSettings, OwnerName, PM_PERMIT
-from nana.helpers.parser import mention_markdown
+from naruto import naruto, setbot, Command, Owner, BotUsername, DB_AVAILABLE, AdminSettings, OwnerName, PM_PERMIT
+from naruto.helpers.parser import mention_markdown
 
 if DB_AVAILABLE:
-    from nana.modules.database.pm_db import set_whitelist, get_whitelist, set_req, get_req, del_whitelist
+    from naruto.modules.database.pm_db import set_whitelist, get_whitelist, set_req, get_req, del_whitelist
 
 welc_txt = f"""Hello, I'm {OwnerName}'s Userbot.
 Try contacting me by pressing buttons down bellow"""
@@ -18,7 +18,7 @@ BLACKLIST = ["hack", "fuck", "bitch", "pubg", "sex"]
 USER_IN_RESTRICT = []
 
 
-@app.on_message(~filters.me & filters.private & ~filters.bot)
+@naruto.on_message(~filters.me & filters.private & ~filters.bot)
 async def pm_block(client, message):
     if PM_PERMIT:
         if not get_whitelist(message.chat.id):
@@ -42,13 +42,13 @@ async def pm_block(client, message):
         return
 
 
-@app.on_message(filters.me & filters.command(["approve"], Command) & filters.private)
+@naruto.on_message(filters.me & filters.command(["approve"], Command) & filters.private)
 async def approve_pm(_client, message):
     set_whitelist(message.chat.id, True)
     await message.edit("`PM permission was approved!`")
 
 
-@app.on_message(filters.me & filters.command(["revoke", "disapprove"], Command) & filters.private)
+@naruto.on_message(filters.me & filters.command(["revoke", "disapprove"], Command) & filters.private)
 async def revoke_pm_block(_client, message):
     del_whitelist(message.chat.id)
     await message.edit("`PM permission was revoked!`")
@@ -71,11 +71,11 @@ async def pm_button(client, query):
             return
         if re.match(r"engine_pm_block", query.data):
             await setbot.edit_inline_text(query.inline_message_id, "💩")
-            await app.send_message(query.from_user.id, "Opps, no nudes for you")
-            await app.block_user(query.from_user.id)
+            await naruto.send_message(query.from_user.id, "Opps, no nudes for you")
+            await naruto.block_user(query.from_user.id)
         elif re.match(r"engine_pm_nope", query.data):
             await setbot.edit_inline_text(query.inline_message_id, "👍")
-            await app.send_message(query.from_user.id,
+            await naruto.send_message(query.from_user.id,
                                 "Hello, please wait for a reply from my master, thank you")
             buttons = InlineKeyboardMarkup([[InlineKeyboardButton("Approve",
                                                                 callback_data=f"engine_pm_apr-{query.from_user.id}"),
@@ -87,28 +87,28 @@ async def pm_button(client, query):
                                     pm_bot_message,
                                     reply_markup=buttons)
             set_req(query.from_user.id, True)
-            from nana.modules.lydia import lydia_status
+            from naruto.modules.lydia import lydia_status
             if lydia_status:
-                await app.send_message(query.from_user.id,
+                await naruto.send_message(query.from_user.id,
                                     "During the wait for permission from my master, why do not we have a little chat?")
         elif re.match("engine_pm_report", query.data):
             await setbot.edit_inline_text(query.inline_message_id, "👍")
-            await app.send_message(query.from_user.id,
-                                "Hello, if you want to report any bugs, please vist in @NanaBotSupport")
+            await naruto.send_message(query.from_user.id,
+                                "Hello, if you want to report any bugs, please vist in @tSupport")
         elif re.match("engine_pm_none", query.data):
             await setbot.edit_inline_text(query.inline_message_id, "👍")
-            await app.send_message(query.from_user.id,
+            await naruto.send_message(query.from_user.id,
                                 "Alright then,\nIf you want anything from me, please contact my again. Thank you")
         elif re.match("engine_pm_apr", query.data):
             target = query.data.split("-")[1]
             await query.message.edit_text(f"[Approved for PM]({target})")
-            await app.send_message(target, "Hello, this is Nana, my master approved you to PM.")
+            await naruto.send_message(target, "Hello, this is titan, my master approved you to PM.")
             set_whitelist(int(target), True)
         elif re.match(r"engine_pm_blk", query.data):
             target = query.data.split("-")[1]
             await query.message.edit_text("That user was blocked~")
-            await app.send_message(target, "Hello, this is Nana, my master has decide to block you.\nSorry for this!")
-            await app.block_user(target)
+            await naruto.send_message(target, "Hello, this is titan, my master has decide to block you.\nSorry for this!")
+            await naruto.block_user(target)
         else:
             await setbot.edit_inline_text(query.inline_message_id, "🙆‍")
     else:
