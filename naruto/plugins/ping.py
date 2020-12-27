e211734c95498ee62a1582d3a3d9d90a608da9d0
edguru <72596185+edguru@users.noreply.github.com>
2020-12-28 04:17:37 +0530

    Update ping.py

--- naruto/plugins/ping.py
+++ naruto/plugins/ping.py
@@ -2,6 +2,6 @@ from . import ping
 from naruto import naruto , AdminSettings , Command
 from pyrogram import filters
 
-@naruto.on_message(filters.command(filters.user(AdminSettings) & ("ping", Command)))
+@naruto.on_message(filters.user(AdminSettings) & filters.command("ping", Command))
 async def _(_, message):
    await ping(_, message)
