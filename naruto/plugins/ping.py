1fde826551fbd60d19c5d6c400812751f4f58d9d
edguru <72596185+edguru@users.noreply.github.com>
2020-12-28 05:09:56 +0530

    Update ping.py

--- naruto/plugins/ping.py
+++ naruto/plugins/ping.py
@@ -3,5 +3,9 @@ from naruto import naruto , AdminSettings , Command
 from pyrogram import filters
 
 @naruto.on_message(filters.user(AdminSettings) & filters.command("ping", Command))
-async def _(_, message):
-   await ping(_, message)
+async def ping_it(_ , message):
+    start_t = time.time()
+    rm = await message.reply_text("...")
+    end_t = time.time()
+    time_taken_s = (end_t - start_t) * 1000
+    await rm.edit(f"Pong!\n{time_taken_s:.3f} ms")
