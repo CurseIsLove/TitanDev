c8b704b3a2ce1c3c538357e72b410fd6357b6c9f
edguru <72596185+edguru@users.noreply.github.com>
2020-12-27 06:19:49 +0530

    Create ping.py

---
+++ naruto/plugins/ping.py
@@ -0,0 +1,10 @@
+import time
+from naruto import naruto
+
+@naruto.on_message(filters.command(filters.user(AdminSettings) & ("ping", Command))
+async def ping(_, message):
+    start_t = time.time()
+    rm = await message.reply_text("...")
+    end_t = time.time()
+    time_taken_s = (end_t - start_t) * 1000
+    await rm.edit(f"Pong!\n{time_taken_s:.3f} ms")
