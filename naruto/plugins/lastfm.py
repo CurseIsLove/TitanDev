49b2cfe31925f4c2c6aa8990609bf1b87ab30ebc
Yash Raj <68998269+DevilYashR@users.noreply.github.com>
2020-12-28 16:05:15 +0530

    Create lastfm.py

---
+++ naruto/plugins/lastfm.py
@@ -0,0 +1,26 @@
+from pyrogram import filters
+
+from naruto import app, Command, AdminSettings
+from naruto.helpers.PyroHelpers import ReplyCheck
+
+__HELP__ = """
+──「 **LastFM** 」──
+-> `lastfm` or `lf`
+Share what you're what listening to with the help of this module!
+Note: you need to go to @lastfmrobot and set your username there
+"""
+__MODULE__ = "Last.FM"
+
+
+@app.on_message(
+    filters.user(AdminSettings) & filters.command(["lastfm", "lf"], Command)
+)
+async def lastfm(client, message):
+    x = await client.get_inline_bot_results("lastfmrobot", "")
+    await message.delete()
+    await message.reply_inline_bot_result(
+        x.query_id,
+        x.results[0].id,
+        reply_to_message_id=ReplyCheck(message),
+        hide_via=True,
+    )
