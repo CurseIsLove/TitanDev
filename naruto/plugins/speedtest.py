9d4869cc73b9f815e66f402cf947d0e369048bdd
Godzilla <72538870+Godzilla-0@users.noreply.github.com>
2020-12-29 17:19:08 +0530

    Update speedtest.py

--- naruto/plugins/speedtest.py
+++ naruto/plugins/speedtest.py
@@ -63,7 +63,7 @@ async def speedtestxyz_callback(client, query):
         )
 
 
-@app.on_message(filters.user(AdminSettings) & filters.command("speedtest", Command))
+@naruto.on_message(filters.user(AdminSettings) & filters.command("speedtest", Command))
 async def speed_test_inline(client, message):
     x = await client.get_inline_bot_results(f"{BotUsername}", "speedtest")
     await message.delete()
