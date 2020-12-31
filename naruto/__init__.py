0e0bfa2c704533f1becfe45b456f399289637fbf
edguru <72596185+edguru@users.noreply.github.com>
2020-12-31 20:19:58 +0530

    Update __init__.py

--- naruto/__init__.py
+++ naruto/__init__.py
@@ -36,6 +36,7 @@ OWNER = Config.OWNER_ID
 Owner = OWNER
 gauth = GoogleAuth()
 GDRIVE_CREDENTIALS = Config.GDRIVE_CREDENTIALS
+gdrive_credentials = Config.GDRIVE_CREDENTIALS
 AdminSettings = Config.AdminSettings
 DB_AVAILABLE = False
 BOTINLINE_AVAIABLE = False
