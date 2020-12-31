852d97e89f519cb668b32d2e12761fac3e6eb7d4
edguru <72596185+edguru@users.noreply.github.com>
2020-12-31 19:38:36 +0530

    Update admincheck.py

--- naruto/helpers/admincheck.py
+++ naruto/helpers/admincheck.py
@@ -1,12 +1,12 @@
 from pyrogram.types import Message
-from naruto import app
+from naruto import naruti
 
 
 async def admin_check(message: Message) -> bool:
     chat_id = message.chat.id
     user_id = message.from_user.id
 
-    check_status = await app.get_chat_member(chat_id=chat_id, user_id=user_id)
+    check_status = await naruto.get_chat_member(chat_id=chat_id, user_id=user_id)
     admin_strings = ["creator", "administrator"]
     # https://git.colinshark.de/PyroBot/PyroBot/src/branch/master/pyrobot/modules/admin.py#L69
     return check_status.status in admin_strings
