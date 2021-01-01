eff6b2f89f26bfb3264d4a2bede1796751e9daac
edguru <72596185+edguru@users.noreply.github.com>
2021-01-02 05:06:59 +0530

    Update logger.py

--- naruto/plugins/logger.py
+++ naruto/plugins/logger.py
@@ -2,10 +2,10 @@ import re
 import asyncio
 
 from pyrogram import filters
-from naruto import naruto , PM_LOGGER
+from naruto import naruto , LOG_CHANNEL_ID
 
 @app.on_message(~filters.me & filters.private & ~filters.bot)
-async def pm_log(_, message):
-    if not PM_LOGGER:
+async def pm_log(client, message):
+    if LOG_CHANNEL_ID is None
         return
-    await message.forward("")
+    await message.forward("LOG_CHANNEL_ID")
