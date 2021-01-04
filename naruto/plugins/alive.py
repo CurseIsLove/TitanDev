caa1657cd7547de3a3850fbc81473647d3b2a513
edguru <72596185+edguru@users.noreply.github.com>
2021-01-04 14:16:24 +0530

    Update alive.py

--- naruto/plugins/alive.py
+++ naruto/plugins/alive.py
@@ -4,7 +4,7 @@ from pyrogram import filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 
-from naruto import setbot, AdminSettings, BotUsername, naruto, Command, OwnerUsername
+from naruto import setbot, AdminSettings, BotUsername, naruto, Command, Ownername, OwnerUsername
 from naruto import StartTime
 from naruto.helpers.PyroHelpers import ReplyCheck
 from naruto.assistant.__main__ import dynamic_data_filter
@@ -34,15 +34,20 @@ def get_readable_time(seconds: int) -> str:
 
     return ping_time
 
-
+Alive_pic = "https://telegra.ph/file/096e4e77231ca13b6ff47.mp4"
 @naruto.on_message(filters.user(AdminSettings) & filters.command("alive", Command))
 async def google_search(client, message):
     start_time = time.time()
     uptime = get_readable_time((time.time() - StartTime))
-    reply_msg = f"{OwnerUsername}@titan\n"
+    reply_msg = f"**TITAN USERBOT** serving \n                 {Ownername}@titan\n"
     reply_msg += "------------------\n"
     end_time = time.time()
     ping_time = round((end_time - start_time) * 1000, 3)
     reply_msg += f"Ping: {ping_time}ms\n"
-    reply_msg += f"Userbot uptime: {uptime}"
-    await message.edit(reply_msg)
+    reply_msg += f"Userbot uptime: {uptime}\n"
+    reply_msg += f"Owners username : {Ownerusername}\n"
+    reply_msg += f"__Running on pyrogram__"
+    reply_msg += f"**Pyhton version**  : 3.8"
+    reply_msg += f"Servers functioning- normal"
+    await client.send_file(message.chat.id , Alive_pic , reply_msg)
+    await message.delete()
